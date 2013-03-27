ffb825627d445a4996ca4095e145e946af3fc73a
newlong <newlongg@gmail.com>
2013-03-27 16:45:28 +0800

    update weibo plugins and update emacs  plugins root-path variable

--- dragon.el
+++ dragon.el
@@ -1,6 +1,9 @@
 (global-set-key (kbd "C-SPC") nil)
 
-(add-to-list 'load-path "~/emacs_plugins/weibo.emacs")
+(defvar config-root "/home/dragon/emacs_plugins/emacs_config/")
+
+;;;加载新浪微博
+(add-to-list 'load-path (concat config-root "weibo_emacs"))
 (require 'weibo)
 
 ;;;最大化屏幕
@@ -24,19 +27,19 @@
 ;;;(my-fullscreen)
 
 ;;; show line number
-(load-file "/home/dragon/emacs_plugins/line_num/line-num.el")
-(load-file "/home/dragon/emacs_plugins/line_num/linum+.el")
+(load-file (concat config-root "line_num/line-num.el"))
+(load-file (concat config-root "line_num/linum+.el"))
 (require 'linum)
 (global-linum-mode 1)
 
 ;;; set php mode
-(load-file "/home/dragon/emacs_plugins/php/php-mode.el")
+(load-file (concat config-root "php/php-mode.el"))
 (require 'php-mode)
 
 ;;; color theme
-(add-to-list 'load-path "/home/dragon/emacs_plugins/color-theme")
-(load-file "/home/dragon/emacs_plugins/color-theme/color-theme.el")
-(load-file "/home/dragon/emacs_plugins/color-theme/color-theme-sunburst.el")
+(add-to-list 'load-path (concat config-root "color-theme"))
+(load-file (concat config-root "color-theme/color-theme.el"))
+(load-file (concat config-root "color-theme/color-theme-sunburst.el"))
 (require 'color-theme)
 (color-theme-initialize)
 (color-theme-tm)
@@ -45,10 +48,10 @@
 (require 'hl-line)
 (global-hl-line-mode 1)
 
-(load-file "/home/dragon/emacs_plugins/myhighline.el")
+(load-file (concat config-root "myhighline.el"))
 
-(add-to-list 'load-path "/home/dragon/emacs_plugins/xcscope")
-(load-file "/home/dragon/emacs_plugins/xcscope/xcscope.el")
+(add-to-list 'load-path (concat config-root "xcscope"))
+(load-file (concat config-root "xcscope/xcscope.el"))
 (require 'xcscope)
 (setq cscope-do-not-update-database t)
 
@@ -59,9 +62,9 @@
 (setq current-path (getenv  "PWD"))
 (setq frame-title-format (concat  " (" current-path ") " "%b" " @emacs24"))
 
-(load-file "/home/dragon/emacs_plugins/flymake_language.el")
+(load-file (concat config-root "flymake_language.el"))
 
 (setq inferior-lisp-program "/usr/local/sbcl/bin/sbcl")
-(add-to-list 'load-path "/home/dragon/emacs_plugins/slime-2012-11-14/")
+(add-to-list 'load-path (concat config-root "slime-2012-11-14/"))
 (require 'slime)
 (slime-setup)
